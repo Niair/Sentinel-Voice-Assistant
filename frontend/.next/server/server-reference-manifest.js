1da8dff907e38a6e04bc5ea7cf2205765a839b4f
Niair <niair2180@gmail.com>
2026-01-18 16:10:17 +0530

    phase - 1.4

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"00oXGbC68kp3pWZsj3CyzKbpgSQaUJdpWwKLg4RopFE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FQQFr8wP2XsAGgtiTAA0vU+MURQtmuVtCrCWwn559TY=\"\n}"
